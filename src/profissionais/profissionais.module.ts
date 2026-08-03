85c743311bb2746b0ab4fbbf5a870e69f4a5dad6
hasbuen <juliocesar.ovidiobueno@outlook.com>
2026-08-03 16:35:53 -0300

    fix: wire professional auth module

--- src/profissionais/profissionais.module.ts
+++ src/profissionais/profissionais.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { ProfissionaisController } from './profissionais.controller';
 import { ProfissionaisService } from './profissionais.service';
 
-@Module({ controllers: [ProfissionaisController], providers: [ProfissionaisService] })
+@Module({ imports: [AuthModule], controllers: [ProfissionaisController], providers: [ProfissionaisService] })
 export class ProfissionaisModule {}
