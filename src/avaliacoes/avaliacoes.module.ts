386447368105b06fa98abafbf6551decd5be6525
hasbuen <juliocesar.ovidiobueno@outlook.com>
2026-08-08 10:39:24 -0300

    fix avaliacoes auth module dependency

--- src/avaliacoes/avaliacoes.module.ts
+++ src/avaliacoes/avaliacoes.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AvaliacoesController } from './avaliacoes.controller';
 import { AvaliacoesService } from './avaliacoes.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AvaliacoesController],
   providers: [AvaliacoesService],
   exports: [AvaliacoesService],
